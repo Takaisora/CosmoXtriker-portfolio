2246bcf7abb3b7ea283818b5615cdfc52a73e4d7
tokyotech-hosyu <tokyotech-hosyu@4900.co.jp>
2019-09-09 16:31:27 +0900

    sdada

---
+++ Animatoin等/CorvetteAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorvetteAnimation : MonoBehaviour
+{
+    /*
+     * private Vector3 vel3;
+    [SerializeField]
+    GameObject wavecon;
+    [SerializeField]
+    GameObject CorvetteEvent1;
+    */
+    Animator animator;
+
+    GameObject Corvettelaser;
+
+    LaserAnimation script;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Corvettelaser = GameObject.Find("Corvette_Laser");
+        script = Corvettelaser.GetComponent<LaserAnimation>();
+    }
+
+    // Update is called once per frame
+    public void EndAnimation()
+    {
+        animator.SetTrigger("St Ani End");
+        script.Laser();
+    }
+}
---
+++ Wave等/Wave3.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave3 : MonoBehaviour
+{
+    // Debri収納
+    GameObject Debri;
+    // Corvette Wave3時
+    [SerializeField]
+    GameObject Corvette3;
+    [SerializeField]
+    GameObject CorvetteEvent3;
+    // Corvette Wave6時
+    [SerializeField]
+    GameObject Corvette6;
+    [SerializeField]
+    GameObject CorvetteEvent6;
+    //Asteroid Event
+    [SerializeField]
+    GameObject Asteroid;
+    // Waveプレハブを収納する
+    public GameObject[] waves;
+    //現在のWave
+    private int currentWave;
+
+
+
+    void Start()
+    {
+        this.Debri = GameObject.Find("Debri Sponner");
+
+        StartCoroutine(StartWave());
+    }
+
+    IEnumerator StartWave()
+    {
+        while (waves.Length >= currentWave)
+        {
+            if (currentWave == 1)
+            {
+                //シーンにいるCorvetteのAnimatorを起動
+                Corvette3.GetComponent<Animator>().enabled = true;
+                Corvette3.GetComponent<Animator>().SetBool("Corvette3", true); 
+
+                /*//Animatorの終わりでCorvette3.csをActiveにするので、それまで待機
+                while (Corvette3.GetComponent<Corvette>().enabled == false)
+                {
+                    yield return new WaitForEndOfFrame();
+                }*/
+            }
+            if (currentWave == 2)
+            {
+                //corvette Event ProのAnimator起動
+                CorvetteEvent3.GetComponent<Animator>().enabled = true;
+                Corvette3.GetComponent<Animator>().SetBool("Corvette3", false);
+            }
+            if (currentWave == 5)
+            {
+                //シーンにいるCorvetteのAnimatorを起動
+                Corvette6.GetComponent<Animator>().enabled = true;
+                Corvette6.GetComponent<Animator>().SetBool("Corvette6", true);
+                Corvette3.GetComponent<Animator>().SetTrigger("Stege out");
+            }
+            if (currentWave == 6)
+            {
+                //corvette Event ProのAnimator起動
+                Corvette6.GetComponent<Animator>().SetBool("Corvette6", false);
+                CorvetteEvent6.GetComponent<Animator>().enabled = true;
+            }
+
+            //Waveを作成する,プレイヤーのポジションｘとｙと同じ講座
+            GameObject wave = (GameObject)Instantiate(waves[currentWave], new Vector3(PlayerController.instance.transform.position.x,PlayerController.instance.transform.position.y,waves[currentWave].transform.position.z)
+            , Quaternion.identity);
+
+            //WaveをEmitterの子要素にする
+            wave.transform.parent = transform;
+
+            //Waveが5になったらDebrisPopをオンに
+            if (currentWave == 4)
+            {
+                this.Debri.GetComponent<DebrisPop>().enabled = true;
+            }
+            //AsteroidEventPro 仮設置
+            if ( currentWave == 3)
+            {
+                Asteroid.GetComponent<Animator>().enabled = true;
+            }
+            //Waveが9になったらDebrisPopをオフに
+            if (currentWave == 8)
+            {
+                this.Debri.GetComponent<DebrisPop>().enabled = false;
+                Corvette6.GetComponent<Animator>().SetTrigger("Stege out");
+            }
+            
+            
+            //Waveの子要素のEnemyがすべて消去されるまで待機する
+            while (wave.activeSelf == true)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+            //Waveの削除
+            Destroy(wave);
+
+            //格納されているWaveをすべて実行したらcurrentWaveを0にする（最初から->ループ）
+            currentWave++;
+            /*
+            if (waves.Length <= currentWave)
+            {
+                currentWave = 0;
+            }*/
+            if(currentWave == 10)
+            {
+                break;
+            }
+
+            yield return null;
+        }
+        /* Destroyer起動 */
+    }
+}
